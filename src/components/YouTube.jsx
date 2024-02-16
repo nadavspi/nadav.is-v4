ca1473fee7cde1d7ef9d4006408bc136492654a4
Nadav Spiegelman <me@nadav.name>
2024-02-15 20:38:15 -0500

    Set up /watching

---
+++ src/components/YouTube.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Embed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+const YouTube = (props) => <Embed {...props} />;
+
+export default YouTube;
