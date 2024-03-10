f297fe61c727dd6deaec5703fc9d4365db11140c
Nadav Spiegelman <me@nadav.name>
2024-03-10 01:07:12 -0500

    Refactor out navLinks

---
+++ src/components/navLinks.js
@@ -0,0 +1,7 @@
+export default [
+  { href: "/notes", text: "Notes" },
+  { href: "/photography", text: "Photography" },
+  { href: "/reading", text: "Reading" },
+  { href: "/watching", text: "Watching" },
+  { href: "mailto:fresh.rain2888@nadav.name", text: "Contact" },
+]
